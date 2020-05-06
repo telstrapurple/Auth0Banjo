137dff0ae06f7b033d412d18d8cf33afe10f12e7
Anthony Attwood <anthony.attwood@readify.net>
2020-05-06 12:36:07 +0800

    doc comment on Reflectorisor

--- Banjo.CLI/Services/Reflectorisor.cs
+++ Banjo.CLI/Services/Reflectorisor.cs
@@ -5,6 +5,18 @@ namespace Banjo.CLI.Services
 {
     public class Reflectorisor
     {
+        /// <summary>
+        /// Copy properties from an instance of one type to a new instance of another type.
+        /// Matches properties by name, then by return/parameter type. That is, the setter has to have exactly one
+        /// parameter and its type has to match the getters return type.
+        /// Requires a public setter on the <c>To</c> property and a public getter on the <c>From</c> property.
+        /// Only performs reference copy, does not deep copy or clone the property values.
+        /// Creates a new instance of the <c>To</c> type.
+        /// </summary>
+        /// <param name="from">the instance to copy properties from</param>
+        /// <typeparam name="From">The type of the instance to copy properties from</typeparam>
+        /// <typeparam name="To">The type of the required return type</typeparam>
+        /// <returns>A new instance of the To type with the matching properties copied from <c>from</c></returns>
         public static To CopyMembers<From, To>(From from) where To : new()
         {
             var to = new To();
